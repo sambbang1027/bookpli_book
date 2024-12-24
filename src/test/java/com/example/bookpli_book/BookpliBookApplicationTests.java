dc8d587c51bc8fd1c67fd6514d30e193aac26be7
Sammy <seosi97@gmail.com>
2024-12-24 11:55:16 +0900

    init

---
+++ src/test/java/com/example/bookpli_book/BookpliBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookpli_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookpliBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
